ceb79376f710c8c7d3a7be3e3ca1effcba4c2ac6
Brad Alexander <brad.alexander913@gmail.com>
2019-08-25 23:31:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-  def nameis(dog_name) 
+  def name =(dog_name) 
     @this_dogs_name = dog_name 
   end 
   
@@ -10,6 +10,6 @@ class Dog
 end 
 
 lassie = Dog.new 
-lassie.nameis( "Lassie") 
+lassie.name = "Lassie" 
 
 lassie.name
